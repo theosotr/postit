09c98d34062c27968175e0da3bd35e541410e82e
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-10-14 02:42:17 +0300

    Revert "add pagination"
    This reverts commit 9cd3e76c735ad782c4db0d00b02dd7301e09096e.

--- postit_project/settings.py
+++ postit_project/settings.py
@@ -27,11 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 300
-}
-
 
 # Application definition
 
